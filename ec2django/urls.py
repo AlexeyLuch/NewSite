f71e8001194838c699f89b4a646510209e2e42b0
AlexeyLuch <pochtamoyapochta123@gmail.com>
2020-08-09 14:58:40 +0300

    all from repositore

--- ec2django/urls.py
+++ ec2django/urls.py
@@ -1,25 +1,33 @@
-""" ec2django URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.conf.urls import include, url
+
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings # new
+from django.urls import path, include # new
+from django.conf.urls.static import static #
 
 urlpatterns = [
-   url(r'^admin/', admin.site.urls),
-   url(r'^', include('helloworld.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	path('', include('poll.urls')),
+	path('jet/', include('jet.urls', 'jet')),
+    path('admin/', admin.site.urls),
+	path('accounts/', include('allauth.urls')),
+	path('chat_channel/', include('chat_channel.urls')),
+	# path('ref_pages/', include('ref_pages.urls')),
+
+]
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ poll/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+
+from .models import Question,Phonebase,Tank,Article,Comments,Unique_set
+admin.site.register(Question)
+admin.site.register(Phonebase)
+admin.site.register(Tank)
+admin.site.register(Unique_set)
+
+class ArticleInline(admin.StackedInline):
+    model = Comments
+    extra = 2
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ['article_title','article_text','article_date','article_image']
+    inlines = [ArticleInline]
+    list_filter = ['article_date']
+    list_display = ['article_text']
+
+
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Comments)
+
+
+# class FlatPageAdmin(admin.ModelAdmin):
+#     fieldsets = (
+#         (None, {
+#             'fields': ('url', 'title', 'content', 'sites')
+#         }),
+#         ('Advanced options', {
+#             'classes': ('collapse',),
+#             'fields': ('registration_required', 'template_name'),
+#         }),
+#     )
